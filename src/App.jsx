8d33b9f0a743e2401d6f465134811f16c524052b
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-30 14:29:54 +0530

    landing page in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Landingpage from "./components/Landingpage";
 
 function App() {
   return (
-    <div className="bg-zinc-800 h-screen w-full">
-      <Navbar></Navbar>
+    <div className="bg-zinc-800 h-screen w-full text-white">
+      <Navbar />
+      <Landingpage />
     </div>
   );
 }
---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Landingpage() {
+  return <div>laaaaaaaaaaa</div>;
+}
+
+export default Landingpage;
