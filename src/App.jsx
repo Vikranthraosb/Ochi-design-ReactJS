644b89ec59f187aa60cd5ec4340b01338bb30f54
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-30 16:00:16 +0530

    marque component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Landingpage from "./components/Landingpage";
+import Marque from "./components/Marque";
 
 function App() {
   return (
-    <div className="bg-zinc-800 h-screen w-full text-white">
+    <div className="bg-zinc-800 min-h-screen w-full text-white">
       <Navbar />
       <Landingpage />
+      <Marque />
     </div>
   );
 }
---
+++ src/components/Marque.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Marque() {
+  return (
+    <div className="w-full py-20 bg-[#104e46] rounded-3xl">
+      <div className="text border-y-2 border-zinc-400 flex whitespace-nowrap px-1 items-center overflow-hidden py-1 capitalize">
+        <h1 className="text-[12vw] leading-none uppercase font-semibold -mt-7">
+          We are OCHI
+        </h1>
+        <h1 className="text-[12vw] leading-none uppercase font-semibold -mt-7">
+          We are OCHI
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Marque;
